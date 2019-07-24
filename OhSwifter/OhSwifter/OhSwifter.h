1a2913935453224954778441a1aceac8ae20f390
ohlulu <ohlulu@goonsdesign.com>
2019-07-25 00:02:32 +0800

    [add] [project] swift framwork

---
+++ OhSwifter/OhSwifter/OhSwifter.h
@@ -0,0 +1,19 @@
+//
+//  OhSwifter.h
+//  OhSwifter
+//
+//  Created by ohlulu on 2019/7/25.
+//  Copyright © 2019 ohlulu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OhSwifter.
+FOUNDATION_EXPORT double OhSwifterVersionNumber;
+
+//! Project version string for OhSwifter.
+FOUNDATION_EXPORT const unsigned char OhSwifterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OhSwifter/PublicHeader.h>
+
+
