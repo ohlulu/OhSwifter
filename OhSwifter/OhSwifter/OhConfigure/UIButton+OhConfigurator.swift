6a9ffa4f0b1e27da3ed89a989d0ff67406ab39f1
ohlulu <ohlulu@goonsdesign.com>
2019-08-04 20:28:29 +0800

    [add] [OhConfigurator] with UIButton file

---
+++ OhSwifter/OhSwifter/OhConfigure/UIButton+OhConfigurator.swift
@@ -0,0 +1,11 @@
+//
+//  UIButton+OhConfigurator.swift
+//  OhSwifter
+//
+//  Created by ohlulu on 2019/8/4.
+//  Copyright © 2019 ohlulu. All rights reserved.
+//
+
+extension OhConfigureWrapper where Base: UIButton {
+    
+}
