3aaba0aea2f89ac436bfa513c1b1d35f53230661
ohlulu <ohlulu@goonsdesign.com>
2019-08-07 23:09:48 +0800

    [add] [UIButton+OhConfigurator] file

--- OhSwifter/OhSwifter/OhConfigure/UIButton+OhConfigurator.swift
+++ OhSwifter/OhSwifter/OhConfigure/UIButton+OhConfigurator.swift
@@ -6,6 +6,45 @@
 //  Copyright © 2019 ohlulu. All rights reserved.
 //
 
-extension OhConfigureWrapper where Base: UIButton {
+public extension OhConfigureWrapper where Base: UIButton {
     
+    @discardableResult
+    func image(_ image: UIImage, for state: UIControl.State) -> OhConfigureWrapper {
+        base.setImage(image, for: state)
+        base.setTitle(nil, for: state)
+        return self
+    }
+    
+    @discardableResult
+    func backgroundImage(_ image: UIImage?, for state: UIControl.State) -> OhConfigureWrapper {
+        base.setBackgroundImage(image, for: state)
+        return self
+    }
+    
+    @discardableResult
+    func title(_ title: String, for state: UIControl.State) -> OhConfigureWrapper {
+        base.setImage(nil, for: state)
+        base.setTitle(title, for: state)
+        return self
+    }
+    
+    @discardableResult
+    func attributedTitle(_ title: NSAttributedString?, for state: UIControl.State) -> OhConfigureWrapper {
+        base.setAttributedTitle(title, for: state)
+        return self
+    }
+    
+    @discardableResult
+    func titleColor(_ color: UIColor, for state: UIControl.State = .normal) -> OhConfigureWrapper {
+        base.setTitleColor(color, for: state)
+        return self
+    }
+    
+    @discardableResult
+    func font(_ font: UIFont) -> OhConfigureWrapper {
+        base.titleLabel?.font = font
+        base
+        return self
+    }
+
 }
