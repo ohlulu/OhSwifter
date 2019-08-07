ce6ab402ffd2aee8f29552184818fab956a63f51
ohlulu <ohlulu@goonsdesign.com>
2019-08-07 23:55:31 +0800

    [add] [UITableView+Oh]

---
+++ OhSwifter/OhSwifter/OhConfigurator/UITableView+Oh.swift
@@ -0,0 +1,76 @@
+//
+//  UITableView+Oh.swift
+//  OhSwifter
+//
+//  Created by ohlulu on 2019/8/7.
+//  Copyright © 2019 ohlulu. All rights reserved.
+//
+
+public extension OhConfigureWrapper where Base: UITableView {
+    
+    @discardableResult
+    func dataSource(_ dataSource: UITableViewDataSource) -> OhConfigureWrapper {
+        base.dataSource = dataSource
+        return self
+    }
+    
+    @discardableResult
+    func delegate(_ delegate: UITableViewDelegate) -> OhConfigureWrapper {
+        base.delegate = delegate
+        return self
+    }
+    
+    @discardableResult
+    func regiter<T>(_ cell: T.Type) -> OhConfigureWrapper where T: UITableViewCell {
+        base.register(cell: cell)
+        return self
+    }
+    
+    @discardableResult
+    func separatorStyle(_ separatorStyle: UITableViewCell.SeparatorStyle) -> OhConfigureWrapper {
+        base.separatorStyle = separatorStyle
+        return self
+    }
+    
+    @discardableResult
+    func separatorInset(_ separatorInset: UIEdgeInsets) -> OhConfigureWrapper {
+        base.separatorInset = separatorInset
+        return self
+    }
+    
+    @discardableResult
+    func separatorColor(_ separatorColor: UIColor) -> OhConfigureWrapper {
+        base.separatorColor = separatorColor
+        return self
+    }
+    
+    @discardableResult
+    func allowsSelection(_ allowsSelection: Bool) -> OhConfigureWrapper {
+        base.allowsSelection = allowsSelection
+        return self
+    }
+    
+    @discardableResult
+    func setEditing(_ editing: Bool, animated: Bool) -> OhConfigureWrapper {
+        base.setEditing(editing, animated: animated)
+        return self
+    }
+    
+    enum EstimatedSectionView {
+        case header, footer, row
+    }
+    
+    @discardableResult
+    func estimatedSectionHeight(_ height: CGFloat, for sectionView: EstimatedSectionView) -> OhConfigureWrapper {
+        switch sectionView {
+        case .header:
+            base.estimatedRowHeight = height
+        case .footer:
+            base.estimatedSectionHeaderHeight = height
+        case .row:
+            base.estimatedSectionFooterHeight = height
+        }
+        return self
+    }
+}
+
