03d2dc6e6d4cafa8aadad25442c7d375c31d5585
ohlulu <ohlulu@goonsdesign.com>
2019-08-09 22:01:54 +0800

    [add] [Convenient] datasource, delegate settable

---
+++ OhSwifter/OhSwifter/OhConfigurator/OhConvenient+Delegate.swift
@@ -0,0 +1,23 @@
+//
+//  OhDataSourceConvenient.swift
+//  OhSwifter
+//
+//  Created by ohlulu on 2019/8/9.
+//  Copyright © 2019 ohlulu. All rights reserved.
+//
+
+public protocol SwiftOriginDelegate: class {
+    associatedtype Delegate
+    var delegate: Delegate { get set }
+}
+
+public extension OhConfigureWrapper where Base: SwiftOriginDelegate {
+    func delegate(_ delegate: Base.Delegate) -> OhConfigureWrapper {
+        base.delegate = delegate
+        return self
+    }
+}
+
+extension UITableView: SwiftOriginDelegate { }
+extension UICollectionView: SwiftOriginDelegate { }
+extension UIPickerView: SwiftOriginDelegate { }
