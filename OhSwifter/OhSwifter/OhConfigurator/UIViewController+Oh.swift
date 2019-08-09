c6859637376dc8438ab7e1b11a480d24298057d9
ohlulu <ohlulu@goonsdesign.com>
2019-08-09 21:06:12 +0800

    [add] [UIViewController+Oh]

---
+++ OhSwifter/OhSwifter/OhConfigurator/UIViewController+Oh.swift
@@ -0,0 +1,31 @@
+//
+//  UIViewController+Oh.swift
+//  OhSwifter
+//
+//  Created by ohlulu on 2019/8/9.
+//  Copyright © 2019 ohlulu. All rights reserved.
+//
+
+
+extension OhConfigureWrapper where Base: UIViewController {
+    
+    @discardableResult
+    public func modalPresentationStyle(_ style: UIModalPresentationStyle) -> OhConfigureWrapper {
+        base.modalPresentationStyle = style
+        return self
+    }
+    
+    @discardableResult
+    public func modalTransitionStyle(_ style: UIModalTransitionStyle) -> OhConfigureWrapper {
+        base.modalTransitionStyle = style
+        return self
+    }
+    
+    @discardableResult
+    public func transitioningDelegate(_ delegate: UIViewControllerTransitioningDelegate?) -> OhConfigureWrapper {
+        base.transitioningDelegate = delegate
+        return self
+    }
+    
+}
+
