882fa69ac04b06539ca5377015ca8d75fd673b2e
ohlulu <ohlulu@goonsdesign.com>
2019-08-09 21:28:22 +0800

    [add] [UIDatePicker+Oh]

---
+++ OhSwifter/OhSwifter/OhConfigurator/UIDatePicker+Oh.swift
@@ -0,0 +1,34 @@
+//
+//  UIDatePicker+Oh.swift
+//  OhSwifter
+//
+//  Created by ohlulu on 2019/8/9.
+//  Copyright © 2019 ohlulu. All rights reserved.
+//
+
+extension OhConfigureWrapper where Base: UIDatePicker {
+    
+    @discardableResult
+    func datePickerMode(_ mode: UIDatePicker.Mode) -> OhConfigureWrapper {
+        base.datePickerMode = mode
+        return self
+    }
+    
+    @discardableResult
+    func date(_ date: Date) -> OhConfigureWrapper {
+        base.date = date
+        return self
+    }
+    
+    @discardableResult
+    func maximumDate(_ date: Date) -> OhConfigureWrapper {
+        base.maximumDate = date
+        return self
+    }
+    
+    @discardableResult
+    func minimumDate(_ date: Date) -> OhConfigureWrapper {
+        base.minimumDate = date
+        return self
+    }
+}
