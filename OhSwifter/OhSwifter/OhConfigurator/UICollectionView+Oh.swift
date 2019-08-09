493d00fce4120b29b318ce4220921b4e2d231bf6
ohlulu <ohlulu@goonsdesign.com>
2019-08-09 22:07:42 +0800

    [update] [UICollectionView] confirm delegate and datasource convenient

--- OhSwifter/OhSwifter/OhConfigurator/UICollectionView+Oh.swift
+++ OhSwifter/OhSwifter/OhConfigurator/UICollectionView+Oh.swift
@@ -9,18 +9,6 @@
 extension OhConfigureWrapper where Base: UICollectionView {
     
     @discardableResult
-    func dataSource(_ dataSource: UICollectionViewDataSource) -> OhConfigureWrapper {
-        base.dataSource = dataSource
-        return self
-    }
-    
-    @discardableResult
-    func delegate(_ delegate: UICollectionViewDelegate) -> OhConfigureWrapper {
-        base.delegate = delegate
-        return self
-    }
-    
-    @discardableResult
     func regiter<T>(_ cell: T.Type) -> OhConfigureWrapper where T: UICollectionViewCell {
         base.register(cell: cell)
         return self
