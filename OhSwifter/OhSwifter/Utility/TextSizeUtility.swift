c93113f30b100082e9f398e2d4cfc73ad9f14331
ohlulu <ohlulu@goonsdesign.com>
2019-10-03 14:43:32 +0800

    [add] text size utility

---
+++ OhSwifter/OhSwifter/Utility/TextSizeUtility.swift
@@ -0,0 +1,21 @@
+//
+//  TextSizeUtility.swift
+//  OhSwifter
+//
+//  Created by ohlulu on 2019/10/2.
+//  Copyright © 2019 ohlulu. All rights reserved.
+//
+
+import UIKit
+
+extension String {
+    func textSize(withFont font: UIFont , maxSize: CGSize) -> CGSize {
+        return self.boundingRect(with: maxSize, options: [.usesLineFragmentOrigin], attributes: [.font : font], context: nil).size
+    }
+}
+
+extension UILabel {
+    func size(withMaxSize maxSize: CGSize) -> CGSize {
+        return (text ?? "").textSize(withFont: font, maxSize: maxSize)
+    }
+}
