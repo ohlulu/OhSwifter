b1d28a216ffc835b9d74198326006a9bd42b4425
ohlulu <ohlulu@goonsdesign.com>
2019-10-15 16:20:39 +0800

    [add] UIScreen extension safe area  for bangs

--- OhSwifter/OhSwifter/UIKit/UIScreen+Ext.swift
+++ OhSwifter/OhSwifter/UIKit/UIScreen+Ext.swift
@@ -10,6 +10,18 @@ import UIKit
 
 public extension UIScreen {
     
+    static var safeAreaInset: UIEdgeInsets {
+        guard let window = UIApplication.shared.delegate?.window as? UIWindow else {
+            return .init(edge: 0)
+        }
+        
+        if #available(iOS 11.0, *) {
+            return window.safeAreaInsets
+        } else {
+            return .init(edge: 0)
+        }
+    }
+    
     static var height: Double {
         return Double(UIScreen.heightf)
     }
