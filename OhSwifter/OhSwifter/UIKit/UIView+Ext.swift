65bf7e22e5ff399fc86edc4a9c886c43fba1f108
ohlulu <ohlulu@goonsdesign.com>
2019-11-19 20:24:32 +0800

    [add] comment

--- OhSwifter/OhSwifter/UIKit/UIView+Ext.swift
+++ OhSwifter/OhSwifter/UIKit/UIView+Ext.swift
@@ -10,6 +10,11 @@ import UIKit
 
 public extension UIView {
     
+    /// duration defaule -> 0.25s,
+    /// delay: defaule -> 0s
+    /// - Parameter duration: defaule -> 0.25s
+    /// - Parameter delay: defaule -> 0s
+    /// - Parameter completion: completed closure
     func fadeOut(duration: TimeInterval = 0.25,
                 delay: TimeInterval = 0,
                 _ completion: ((Bool) -> Void)? = nil) {
@@ -18,6 +23,11 @@ public extension UIView {
         }, completion: completion)
     }
     
+    /// duration defaule -> 0.25s,
+    /// delay: defaule -> 0s
+    /// - Parameter duration: defaule -> 0.25s
+    /// - Parameter delay: defaule -> 0s
+    /// - Parameter completion: completed closure
     func fadeIn(duration: TimeInterval = 0.25,
                 delay: TimeInterval = 0,
                 _ completion: ((Bool) -> Void)? = nil) {
@@ -26,9 +36,12 @@ public extension UIView {
         }, completion: completion)
     }
 
+    /// Animated with a horizontal shake, duration -> 0.35s
+    /// - duration -> 0.35s
+    /// - offsets -> [-10.0, 10.0, -10.0, 10.0, -5.0, 5.0, 0.0]
     func errorshake(completion:(() -> Void)? = nil) {
         shake(duration: 0.35,
-              offsets: [-10.0, 10.0, -10.0, 10.0, -5.0, 5.0, 0.0 ],
+              offsets: [-10.0, 10.0, -10.0, 10.0, -5.0, 5.0, 0.0],
               completion: completion)
     }
     
